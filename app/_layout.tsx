df778320b9a87fab99f7cf2c6ccb59d5abf7ce26
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-07-10 12:51:56 +0200

    feat : add authenticaton provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Slot } from 'expo-router';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import AuthProvider from './providers/AuthProvider';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,10 +19,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+     <AuthProvider>
+      <Slot/>
+     </AuthProvider>
     </ThemeProvider>
   );
 }
